5d8b308823cb4464010a0f31f220ac8aaae5ae6f
karthik <karthik.nishanth06@gmail.com>
2024-11-11 12:43:50 +0000

    form Change

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -1,11 +1,22 @@
 "use client";
 
-import ContactForm from "@/components/form";
-
 const ContactPage = () => {
   return (
     <div className="container mx-auto px-4 py-12">
-      <ContactForm />
+      <h1 className="text-4xl font-bold mb-4 text-center">
+        Get your Website Ready for 2025
+      </h1>
+      <iframe
+        src="//profici.co.uk/gfembed/?f=7"
+        width="70%"
+        height="1200"
+        frameBorder="1"
+        className="gfiframe mx-auto block border-2 p-2 border-black rounded-lg"
+      />
+      <script
+        src="//profici.co.uk/wp-content/plugins/gravity-forms-iframe-master/assets/scripts/gfembed.min.js"
+        type="text/javascript"
+      />
     </div>
   );
 };
